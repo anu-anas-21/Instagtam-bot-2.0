ccabdd7947a2f980f5610353477bd5b3d572af88
MOHAMMED ANAS <64194917+anu-anas-21@users.noreply.github.com>
2022-12-20 21:38:34 +0530

    Add files via upload

---
+++ AnasBot.py
@@ -0,0 +1,18 @@
+from time import perf_counter
+from instapy import InstaPy
+
+#Login Section
+session = InstaPy(username="anu._.anas", password="aNASmE04082001", headless_browser=True)
+session.login()
+
+#Like Section
+session.like_by_tags(["javascript","python"],amount=6)
+
+#Follow Section
+session.set_do_follow(True, percentage=25)
+
+#Comment Section
+session.set_do_comment(True, percentage=50)
+session.set_comments(["Sun kissed" , "Lovely" , "Keep it up"])
+
+session.end()
---
+++ insta_bot.py
@@ -0,0 +1,23 @@
+from time import sleep
+from turtle import delay
+from instabot import Bot
+
+my_bot = Bot()
+
+
+my_bot.login(username="anu._.anas" , password="aNASmE04082001")
+
+#unfollow section
+my_bot.unfollow_non_followers()
+delay(2)
+
+#like section
+my_bot.like_hashtag(["bmw","lulu mall","kerala"], amount=100)
+sleep(30)
+
+#follow section
+user_id=my_bot.get_user_id_from_username(["tfm._play","team_kuttusan"])
+my_bot.follow_followers(user_id, nfollows=30)
+sleep(20)
+
+my_bot.logout()
